20273238fdf78402afc43f544728bfaa8209b5b0
Sopa Tsewang <sopa.tsewang@Sopas-MacBook-Pro.local>
2024-05-23 23:58:37 -0400

    learn function, venv

---
+++ Day-04/advance_calc.py
@@ -0,0 +1,3 @@
+import calculator as basic_calc
+
+basic_calc.add()
---
+++ Day-04/calculator.py
@@ -0,0 +1,23 @@
+a = 10
+b = 5
+
+def add(a, b):
+    add = a + b
+    return add
+
+def sub(a, b):
+    sub = a - b
+    return sub
+
+def mul(a, b):
+    mul = a * b
+    return mul
+
+def div(a, b):
+    div = a / b
+    return div
+
+print(add(5, 1))
+print(sub(10, 5))
+print(mul(5, 10))
+print(div(10, 5))
